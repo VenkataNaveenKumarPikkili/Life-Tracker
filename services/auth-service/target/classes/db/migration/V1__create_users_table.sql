4ebab44d42d7870adfaa61602aec5e6ec1a046b5
NaveenKrishna <venkatanaveenkumarpikkili@gmail.com>
2025-11-21 17:54:50 +0000

    auth-service updated

--- services/auth-service/target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
